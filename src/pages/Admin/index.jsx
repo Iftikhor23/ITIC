2f32bc3b47cc66561eebcbfd64504e4a27b4a4e5
Ulugbek121 <yoqubjanov1991@gmail.com>
2023-09-27 17:07:10 +0500

    done almost admin page

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import AdminNavbar from '../../components/AdminNavbar'
-import AdminSidebar from '../../components/AdminSidebar/Sidebar'
+import React from "react";
+import AdminNavbar from "../../components/AdminNavbar";
+import AdminSidebar from "../../components/AdminSidebar/Sidebar";
+import { Outlet } from "react-router-dom";
+import Loader from "../../components/Loader";
 
 const Admin = () => {
   return (
     <div>
-      <AdminNavbar/>
-      <AdminSidebar/>
+      <AdminNavbar />
+      <div className="FlexBoxAdmin">
+        <AdminSidebar />
+        <Loader>
+          <Outlet />
+        </Loader>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
