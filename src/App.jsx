b269e4a2cca55ee8d71eb37cdc22e2e97a3b3290
Ulugbek121 <yoqubjanov1991@gmail.com>
2023-09-27 17:52:15 +0500

    done almost admin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AllCases from './pages/AllCases/AllCases'
-import Pages from './pages/index';
+import AllCases from "./pages/AllCases/AllCases";
+import Pages from "./pages/index";
 import AllVacancies from "./pages/AllVacancies/AllVacancies";
-import Sidebar from './components/AdminSidebar/Sidebar';
 import Admin from "./pages/Admin";
 import { Data } from "./components/AdminSidebar/SidebarData";
-import AdminHome from "./pages/Admin/Home";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/allCases" element={<AllCases/>}/>
-          <Route path="/allVacancies" element={<AllVacancies/>}/>
-          <Route path="/" element={<Pages/>}/>
-          <Route path="/admin" element={<Admin/>}>
-          {Data?.map(({ path, component }, i) => (
-            <Route exact path={path} element={component} key={i} />
-          ))}
+          <Route path="/allCases" element={<AllCases />} />
+          <Route path="/allVacancies" element={<AllVacancies />} />
+          <Route path="/" element={<Pages />} />
+          <Route path="/admin" element={<Admin />}>
+            {Data?.map(({ path, component }, i) => (
+              <Route exact path={path} element={component} key={i} />
+            ))}
           </Route>
         </Routes>
       </BrowserRouter>
--- src/pages/Admin/styled.js
+++ src/pages/Admin/styled.js
@@ -1,44 +1,35 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-<<<<<<< HEAD
   width: 100%;
   height: 100%;
+  &.ColumnBox {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 25px;
+  }
+  &.TagBox {
+    width: 100%;
+    height: 50px;
+    border-radius: 100px;
+    background: #f4f7f4;
+    display: flex;
+    align-items: center;
+    padding: 4px;
+    gap: 5px;
+  }
+  p {
+    &.Header {
+      color: #000;
+      font-size: 24px;
+      font-style: normal;
+      font-weight: 500;
+      line-height: normal;
+    }
+  }
 `;
-=======
-	width: 100%;
-	height: 100%;
-	div {
-		&.FlexBoxAdmin {
-			display: flex;
-			height: 100%;
-		}
-		&.ColumnBox {
-			width: 100%;
-			display: flex;
-			flex-direction: column;
-			gap: 25px;
-			/* padding: 20px 0 0 20px; */
-		}
-		&.TagBox {
-			width: 100%;
-			height: 50px;
-			border-radius: 100px;
-			background: #f4f7f4;
-			display: flex;
-			align-items: center;
-			padding: 4px;
-			gap: 5px;
-		}
-	}
-	p {
-		&.Header {
-			color: #000;
-			font-size: 24px;
-			font-style: normal;
-			font-weight: 500;
-			line-height: normal;
-		}
-	}
+Wrapper.FlexBoxAdmin = styled.div`
+  display: flex;
+  height: 100%;
 `;
->>>>>>> 28cfa7e498cb1615a3cf06030542a5b9f928b508
