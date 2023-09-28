9f281eda369c8efb3fa24e8e41df0ac756c1c8c7
Iftikhor23 <113969087+Iftikhor23@users.noreply.github.com>
2023-09-28 12:21:37 +0500

    changes in case

---
+++ src/pages/Admin/Cases/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    width: 100%;
+    height: 100%;
+`;
+
+Wrapper.Wrap = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    gap: 24px;
+`;
+
