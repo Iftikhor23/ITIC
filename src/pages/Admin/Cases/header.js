780e84633e4630c431cea3f44ef769e2c96c7573
Ulugbek121 <yoqubjanov1991@gmail.com>
2023-10-03 15:26:59 +0500

    sekin sekin bovotti

--- src/pages/Admin/Cases/header.js
+++ src/pages/Admin/Cases/header.js
@@ -1,18 +1,76 @@
+import { useCaseContext } from "../../../context/CaseContext";
 import { Wrapper } from "./styles";
 import { useNavigate } from "react-router-dom";
-const Header = () => {
-  return (
-    <Wrapper.Flex>
-      <Wrapper.Box>{/* <TrashIcon /> */}</Wrapper.Box>
-      <Wrapper.Box>{/* <PenIcon /> */}</Wrapper.Box>
-    </Wrapper.Flex>
-  );
+import request from "../../../services";
+import Popup from "../../../components/Reusable/Popup";
+import Toast from "../../../components/Reusable/Toast";
+import { PenIcon, TrasIcon } from "../../../components/Reusable/GenericIcons/genericIcons";
+
+const ActionRenderer = ({ data }) => {
+	const navigate = useNavigate();
+	const [, dispatch] = useCaseContext();
+
+	const getAdmins = async () => {
+		try {
+			const res = await request.get('admin/directions/all');
+			dispatch({
+				type: 'setDirections',
+				payload: res?.data?.data,
+			});
+		} catch (error) {
+			Popup({
+				title: error?.response?.data?.resultMsg,
+				type: 'warning',
+			});
+		}
+	};
+	const deleteF = async () => {
+		try {
+			const res = await request.delete(`/admin/directions/${data?.id}`);
+			getAdmins();
+			Toast({
+				type: 'success',
+				message: 'Deleted !',
+			});
+		} catch (error) {
+			Popup({
+				title: error?.response?.data?.resultMsg,
+				type: 'warning',
+			});
+		}
+	};
+
+	const deleteFunc = async () => {
+		Popup({
+			title: 'Do you want to delete this direction ?',
+			isConfirmedFunction: () => deleteF(),
+			showCancelButton: true,
+			type: 'question',
+		});
+	};
+	const editFunc = () => {
+		dispatch({
+			type: 'setSelected',
+			payload: data,
+		});
+		navigate(`/admin/direction/edit/:${data?.id}`);
+	};
+	return (
+		<Wrapper.Flex>
+			<Wrapper.Box onClick={deleteFunc}>
+				<TrasIcon />
+			</Wrapper.Box>
+			<Wrapper.Box onClick={editFunc}>
+				<PenIcon />
+			</Wrapper.Box>
+		</Wrapper.Flex>
+	);
 };
 
 export const column = [
   {
     headerName: "Cover",
-    
+	cellRender: ActionRenderer,
     flex: 0.5,
   },
 
