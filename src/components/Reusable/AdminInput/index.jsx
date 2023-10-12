d2e7909f357b58dbe5e30d8d43c11a1719b0bc33
Iftikhor23 <113969087+Iftikhor23@users.noreply.github.com>
2023-10-12 14:08:54 +0500

    trying

--- src/components/Reusable/AdminInput/index.jsx
+++ src/components/Reusable/AdminInput/index.jsx
@@ -10,7 +10,7 @@ function AdminInput({ label, placeholder, prefix, onChange,value}) {
 
   return (
     <Container>
-      <Wrapper.Label lbcolor={lbcolor}>{label}</Wrapper.Label>
+      <Wrapper.Label >{label}</Wrapper.Label>
       <Wrapper>
         <Container.Prefix>{prefix}</Container.Prefix>
         <Wrapper.Input
--- src/pages/AdminSign/index.jsx
+++ src/pages/AdminSign/index.jsx
@@ -2,17 +2,56 @@ import { Container } from "./styles";
 import AdminInput from "../../components/Reusable/AdminInput";
 import logo from "../../assets/images/itLogo.svg";
 import ButtonComb from "../../components/Reusable/ButtonComb/Button";
+import request from "../../services";
+import { useRef, useState, useEffect } from "react";
 
 function AdminSign() {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [errMsg, setErrMsg] = useState();
+
+  useEffect(() => {
+    useRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, password]);
+
   return (
     <Container>
       <Container.Wrapper>
+        <p
+          ref={errRef}
+          className={errMsg ? "errmsg" : "offscreen"}
+          aria-live="assertive"
+        >
+          {errMsg}
+        </p>
         <Container.Header>
           <img src={logo} alt="logo" />
           <p className="headerText">Build Your Future With Us</p>
         </Container.Header>
-        <AdminInput label={"Email"} placeholder={"Enter email"} lbcolor={"#fff"}/>
-        <AdminInput label={"Password"} placeholder={"Enter password"} lbcolor={"#fff"}/>
+        <AdminInput
+          label={"Email"}
+          placeholder={"Enter email"}
+          ref={userRef}
+          onChange={(e) => setUser(e.target.value)}
+          value={user}
+          required
+        />
+
+        <AdminInput
+          label={"Password"}
+          placeholder={"Enter password"}
+          ref={userRef}
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          required
+        />
         <ButtonComb
           btnheight="60px"
           btnwidth="200px"
