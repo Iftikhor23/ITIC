7cbf9ffcf9cbb52672fed0110af564b4b2a084f0
Ulugbek121 <yoqubjanov1991@gmail.com>
2023-10-16 11:17:50 +0500

    bismillah

--- src/pages/Admin/Resume/Resume.jsx
+++ src/pages/Admin/Resume/Resume.jsx
@@ -3,9 +3,30 @@ import { column } from './header';
 import Table from "../../../components/Reusable/CustomTable";
 import AdminSearch from "../../../components/AminSearch";
 import RegularButton from '../../../components/Reusable/RegularButton'
+import request from "../../../services";
+import { useResumeContext } from "../../../context/ResumeContext";
+import { useEffect } from "react";
 
 const  Resume = () => {
+  const [{ resumedata }, dispatch] = useResumeContext();
 
+
+  const getResume = async () => {
+    try {
+      const res = await request.get(`admin/resume/all`);
+      dispatch({
+        type: "setResume",
+        payload: res?.data?.data,
+      });
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
+  console.log(resumedata);
+
+  useEffect(() => {
+    getResume();
+  }, []);
   return (
     <Wrapper>
       <Wrapper.Wrap>
@@ -16,7 +37,7 @@ const  Resume = () => {
           <RegularButton title="Vacancy applies" btnwidth={"141px"} btnheight={"42px"}/>
         </Wrapper.Nav>
         <Wrapper.WrapTable>
-          <Table column={column} />
+          <Table column={column} rowData={resumedata} />
         </Wrapper.WrapTable>
       </Wrapper.Wrap>
     </Wrapper>
--- src/pages/Admin/Resume/styles.js
+++ src/pages/Admin/Resume/styles.js
@@ -33,3 +33,80 @@ Wrapper.WrapTable = styled.div`
   width: 100%;
   height: 95%;
 `;
+Wrapper.Flex = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  .CVlink {
+    color: #009299;
+    text-overflow: ellipsis;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
+`;
+Wrapper.FaqtitleBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 15px;
+  padding: 30px 0 19px 0;
+
+  .faqtitle {
+    color: #000;
+    font-family: SF Pro "Display";
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+  }
+  .faqdesc {
+    color: #000;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 21px;
+    word-wrap: break-word;
+    width: 1100px;
+  }
+  .casetitle-link {
+    color: #000;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 21px;
+    word-wrap: break-word;
+  }
+`;
+Wrapper.BoxIcon = styled.div`
+  display: flex;
+  width: 33px;
+  height: 33px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  background-image: ${({ bgImage }) => bgImage || "none"};
+  cursor: pointer;
+  border: 1px solid
+    ${({ type }) =>
+      type === "comment" ? "#EEE" : type === "close" ? "#FF5151" : "#37A67E"};
+`;
+
+Wrapper.Comment = styled.div`
+  position: absolute;
+  display: ${({ hover }) => (hover ? "flex" : "none")};
+  width: 356px;
+  padding: 20px;
+  border-radius: 12px;
+  border: 1px solid #e1e1e1;
+  background: #fff;
+  z-index: 1;
+  line-height: 20px;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  left: 973px;
+  top: 37px;
+  color: #000;
+  transition: 0.5s ease;
+`;
